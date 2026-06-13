a1175d833c45a5a95f5d4e41d49bb14edfc7e95f
vlad-j <38561464+vlad-j@users.noreply.github.com>
2026-06-13 19:06:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Clear all caches and pass through all requests
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
